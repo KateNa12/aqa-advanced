a74704c100ada9db262cd5545c4024d8e5600ee6
KateNa12 <60751131+KateNa12@users.noreply.github.com>
2024-10-07 15:04:12 +0300

    Revert "added index.js file as homework #3"

--- nodeBasics/index.js
+++
@@ -1,8 +0,0 @@
-import chalk from 'chalk';
-
-
-console.log(chalk.red('Richard') + chalk.rgb(255, 136, 0).bold(' of') + chalk.yellow(' York') + chalk.green(' Gave') + chalk.cyan(' Battle') + chalk.blue(' in') + chalk.rgb(255,0,0).bold(' vain'))
-//Richard (red) Of (orange) York (yellow) Gave (green) Battle (blue) In (indigo) Vain (violet)
-
-
-
